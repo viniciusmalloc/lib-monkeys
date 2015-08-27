fa45898325795307c027fc83ebf4fbf529e920c0
viniciusmalloc <vini.martins.7@gmail.com>
2015-08-27 17:19:39 -0300

    template modification

--- config/template.cpp
+++ config/template.cpp
@@ -1,24 +1,4 @@
-#include <map> 
-#include <set> 
-#include <list>
-#include <stack>
-#include <cmath> 
-#include <queue> 
-#include <ctime>
-#include <cfloat>
-#include <vector> 
-#include <string> 
-#include <cstdio>
-#include <bitset>
-#include <climits> 
-#include <cstdlib> 
-#include <cstring> 
-#include <cassert> 
-#include <iomanip>
-#include <sstream>
-#include <utility>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 #define FILL(X, V) memset((X), (V), sizeof(X))
 #define TI(X) __typeof((X).begin())
@@ -31,29 +11,38 @@
 #define RREP(i, N) for(int i = N-1; i >= 0; --i)
 #define FORIT(i, a) for(TI(a) i = (a).begin(); i != (a).end(); ++i)
 
-#define PB push_back
-#define MP make_pair
+#define pb push_back
+#define mp make_pair
     
 #define INF 0x3F3F3F3F
 #define LINF 0x3F3F3F3FFFFFFFFFLL
 
 const double EPS = 1e-9;
-inline int SGN(double a) {
-    return ((a > EPS)?(1):((a < -EPS)?(-1):(0)));
+
+int SGN(double a)
+{
+    return ((a > EPS) ? (1) : ((a < -EPS) ? (-1) : (0)));
 }
-inline int CMP(double a, double b) {
+
+int CMP(double a, double b)
+{
     return SGN(a - b);
 }
 
 typedef long long int64;
 typedef unsigned long long uint64;
+typdef pair<int, int> ii;
+
+struct node{
+    int a, b;
+    node (int a = 0, int b = 0) : a(a), b(b) {}
+};
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-	ios::sync_with_stdio(false);
-	
-	
-	return 0;
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
 
+    return 0;
 }
